544013a4381df97cdfa587aacb999896295e3a9b
Henry Johnson <jo74705@gmail.com>
2022-01-31 20:54:41 +0800

    Update data

--- javascript_basic_1/app.js
+++ javascript_basic_1/app.js
@@ -1,1 +1,8 @@
-const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+const colors = ['green', 'red', 'rgba(133,122,200)', '#f15025']
+const btn = document.getElementById('clickBtn')
+btn.addEventListener('click', () => {
+  const content = document.getElementById('content')
+  const randomColor = colors[Math.floor(Math.random() * colors.length)]
+  
+  content.style.backgroundColor = randomColor
+})
