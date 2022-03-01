f82da925240e85e4729a3292cef1b4796fcf5790
Henry Johnson <jo74705@gmail.com>
2022-03-01 20:13:01 +0800

    Update promise

--- Promise_example/promise_refactor.js
+++ Promise_example/promise_refactor.js
@@ -1,26 +1,27 @@
 let username = 'nothing'
-function setName () {
-  return new Promise((resolve, reject) => {
+const setName = () => new Promise((resolve, reject) => {
     try {
-      setTimeout(() => {
-        username = 'Henry'
-        console.log('printName time>', new Date().toLocaleTimeString())
-        console.log('The user name was set')
-        resolve('setName successfully')
-      }, 2000)
-    } catch (e) {
-      reject(e)
-    }
+        setTimeout(() => {
+          username = 'Henry'
+          console.log('setName time>', new Date().toLocaleTimeString())
+          console.log('The user name was set')
+          resolve('setName successfully')
+        }, 2000)
+      } catch (e) {
+        reject(e)
+      }
   })
+  
+
+const printName =()=>{
+    console.log('printName time>', new Date().toLocaleTimeString())
+    console.log(`Hi, My name is ${username}`)
 }
-function printName () {
-  console.log('printName time>', new Date().toLocaleTimeString())
-  console.log(`Hi, My name is ${username}`)
-}
-async function init () {
+const init = async ()=>{
     console.log("init start>>>", new Date().toLocaleTimeString())
-  const result = await setName()
-  console.log('result>>', result)
-  printName()
+    const result = await setName() //receive
+    console.log('result>>', result)
+    printName()
 }
+
 init()
